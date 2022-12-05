a81da46cfa2767794971d504811122d9b9a5d79c
Adam Kariv <adam.kariv@gmail.com>
2022-12-05 22:05:02 +0200

    Fix endpoints for prod

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_endpoint: 'http://localhost:5000/api',
-  auth_endpoint: 'http://localhost:5000',
+  api_endpoint: 'https://mapali.datacity.org.il/api',
+  auth_endpoint: 'https://mapali.datacity.org.il',
 };
