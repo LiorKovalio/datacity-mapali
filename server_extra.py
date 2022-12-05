40ea080b43bb6cfad31091998ea1af517d1143d2
Adam Kariv <adam.kariv@gmail.com>
2022-12-05 23:47:04 +0200

    better routes

--- server_extra.py
+++ server_extra.py
@@ -2,13 +2,6 @@ from flask import send_file
 
 def extra_server_init(app):
     @app.route('/')
+    @app.route('/<path:path>')
     def mapali(subpath=None):
-        return send_file(f'ui/dist/ui/index.html')
-
-    @app.route('/new')
-    def mapali_new(subpath=None):
-        return send_file(f'ui/dist/ui/index.html')
-
-    @app.route('/[a-z0-9]{16}')
-    def mapali_map(subpath=None):
         return send_file(f'ui/dist/ui/index.html')
