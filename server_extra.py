5ae4c59702c7b69a43ea179a23e8987a996f55a0
Adam Kariv <adam.kariv@gmail.com>
2022-12-05 22:38:54 +0200

    Route for maps

--- server_extra.py
+++ server_extra.py
@@ -8,3 +8,7 @@ def extra_server_init(app):
     @app.route('/new')
     def mapali_new(subpath=None):
         return send_file(f'ui/dist/ui/index.html')
+
+    @app.route('/[a-z0-9]{16}')
+    def mapali_map(subpath=None):
+        return send_file(f'ui/dist/ui/index.html')
