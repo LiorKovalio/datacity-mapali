7a06dab22b09eba6d24acd6e97063cec5578f444
Adam Kariv <adam.kariv@gmail.com>
2022-12-06 08:17:49 +0200

    New paths for mapali

--- server_extra.py
+++ server_extra.py
@@ -1,7 +1,11 @@
 from flask import send_file
 
 def extra_server_init(app):
-    @app.route('/')
-    @app.route('/<path:path>')
-    def mapali(subpath=None):
+    @app.route('/:id')
+    def mapali_all(id=None):
         return send_file(f'ui/dist/ui/index.html')
+
+    @app.route('/')
+    def mapali():
+        return send_file(f'ui/dist/ui/index.html')
+
